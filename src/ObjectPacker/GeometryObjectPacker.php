eedb221259eecfacadf8d380d63d6ad999985e7a
Benjamin Morel <benjamin.morel@gmail.com>
2017-10-31 23:44:00 +0100

    Add GeometryObjectPacker
    This should really be in a separate package, but will stay there at least until the package stabilizes.

---
+++ src/ObjectPacker/GeometryObjectPacker.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Brick\App\ObjectPacker;
+
+use Brick\Geo\Geometry;
+use Brick\Geo\Exception\GeometryException;
+
+/**
+ * Handles conversion between geometry objects and strings.
+ */
+class GeometryObjectPacker implements ObjectPacker
+{
+    /**
+     * The default SRID to assign to the geometry when reading WKT.
+     *
+     * @var int
+     */
+    private $srid;
+
+    /**
+     * @param int $srid The default SRID to assign to the geometry when reading WKT.
+     */
+    public function __construct(int $srid = 0)
+    {
+        $this->srid = $srid;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function pack($object) : ?PackedObject
+    {
+        if ($object instanceof Geometry) {
+            return new PackedObject(Geometry::class, $object->asText());
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unpack(PackedObject $packedObject)
+    {
+        $class = $packedObject->getClass();
+        $data  = $packedObject->getData();
+
+        if ($class === Geometry::class || is_subclass_of($class, Geometry::class)) {
+            try {
+                $geometry = Geometry::fromText($data, $this->srid);
+
+                if (! $geometry instanceof $class) {
+                    throw new Exception\ObjectNotConvertibleException(sprintf(
+                        'Expected instance of %s, got instance of %s.',
+                        $class,
+                        get_class($geometry)
+                    ));
+                }
+
+                return $geometry;
+            } catch (GeometryException $e) {
+                throw new Exception\ObjectNotConvertibleException($e->getMessage(), 0, $e);
+            }
+        }
+
+        return null;
+    }
+}
